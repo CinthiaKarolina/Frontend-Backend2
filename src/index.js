8c86d9f26a68a94f1d0fc1d4fd4c87c1c11a9a99
CinthiaKarolina <cinthia.k2008@gmail.com>
2025-10-01 18:09:46 -0300

    servidor rodando na porta 3333

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import express from "express"
+
+const app = express()
+const port = 3333
+
+app.get("/", (request, response) => {
+    response.json({ message: "Você acessou o backend!"})
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}!`)
+})
